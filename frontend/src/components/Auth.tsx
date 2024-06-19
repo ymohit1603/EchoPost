4c01672f8ac7accc3c3c1edd1ccbc7cc92e240c7
ymohit1603 <mohityadav0330@gmail.com>
2024-06-20 00:13:41 +0530

    added auth component

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -89,6 +89,6 @@ function LabbeledInput({label,placeholder,onChange,type}:LabelledInputType){
 
     return <div>
     <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-    <input  type={type||"text"} id="first_name" className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} onChange={onChange} required />
+    <input   type={type||"text"} id="first_name" className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} onChange={onChange} required />
 </div>
 }
