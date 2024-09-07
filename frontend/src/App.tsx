0c68d3237af23c350f8a9987fea0ea6e5c5a1324
ymohit1603 <mohityadav0330@gmail.com>
2024-09-07 22:43:37 +0530

    add: multiple routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,11 @@ import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
-import { Blog } from './pages/Blog'
-import { Blogs } from './pages/Blogs';
-import { Publish } from './components/Publish';
+import  Blog  from './pages/Blog'
+import  Blogs  from './pages/Blogs';
+import Publish  from './pages/Publish';
 import LandingPage from './pages/Home';
+import ContactUs from './pages/Contact';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Route path="/signin" element={<Signin />} />
         <Route path="/blog/:id" element={<Blog />} />
         <Route path="/blogs" element={<Blogs />} />
-        <Route path='/publish' element={<Publish/>}></Route>
+        <Route path='/publish' element={<Publish />}></Route>
+        <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </BrowserRouter>
   )
