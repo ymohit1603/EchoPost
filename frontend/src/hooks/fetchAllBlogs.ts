ff37467163968bfcb85a465d3331c78075326959
ymohit1603 <mohityadav0330@gmail.com>
2024-09-16 22:19:44 +0530

    fix: axios req

--- frontend/src/hooks/fetchAllBlogs.ts
+++ frontend/src/hooks/fetchAllBlogs.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const fetchAllBlogs = async () => {
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  console.log(BACKEND_URL);
     try {
-      const response = await axios.get('/api/v1/book/');
+      const response = await axios.get(`${BACKEND_URL}/api/v1/book`);
+      console.log(response);
       return response.data; 
     } catch (error) {
       console.error("Error fetching posts:", error);
